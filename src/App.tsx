985cbe1def451b888f00ddbbcef666db83034541
Nikita Gaponov <nikitagaponov@gmail.com>
2022-11-04 17:47:53 +0300

    04november

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+// import {Rating, RatingValueType} from "./Components/Rating/Rating";
+// import {UnControllableRating} from "./Components/UnControllableRating/UnControllableRating";
+// import {UnControllableAccordion} from "./Components/UnControllableAccordion/UnControllableAccordion";
+// import {Accordion} from "./Components/Accordion/Accordion";
+import {OnOff} from "./Components/OnOff/OnOff";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+    // let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(true)
+    let [onOff, setOnOff] = useState<boolean>(true)
+    return (
+        <div className={"App"}>
+            {/*<PageTitle title={"This is App component"}/>*/}
+            {/*Article 1*/}
+            {/*<span><UnControllableRating/>*/}
+            {/*<UnControllableRating/>*/}
+            {/*<UnControllableRating/>*/}
+            {/*<UnControllableRating/>*/}
+            {/*<UnControllableRating/></span>*/}
+            {/*<hr/>*/}
+            {/*<Accordion titleValue={"Menu"} accordionCollapsed={accordionCollapsed} onClickCollapse={setAccordionCollapsed}/>*/}
+            {/* Article2*/}
+            {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+            {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+            {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+            {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+            {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+            <OnOff onClickOnOff={setOnOff} on={onOff}/>
+            {/*<UnControllableAccordion />*/}
+
+        </div>
+    );
 }
 
 export default App;
